6284fdebb878d911095b90b2cc82423035e34f15
fhr <fenghaoran@camera360.com>
2015-04-14 13:16:58 +0800

    提交一次

---
+++ CCube/CCube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCube
+//
+//  Created by Cc on 15/4/14.
+//  Copyright (c) 2015年 PinguoSDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
