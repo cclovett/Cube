9232275ab75e6cc8f10846832e18bdf53d2778df
fhr <fenghaoran@camera360.com>
2015-07-18 14:14:55 +0800

    Cc

--- CCube/CCube/AppDelegate.m
+++ CCube/CCube/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <pg_edit_sdk_api/pg_edit_sdk_api.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [pg_edit_sdk_api_controller sStart:@""];
     return YES;
 }
 
